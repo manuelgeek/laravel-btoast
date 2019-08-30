8b81d71958f405f642b95ef3b4c5affdaec9755f
manuelgeek <emashmagak@appslab.co.ke>
2019-08-30 22:54:51 +0300

    updated test

--- tests/DemoTest.php
+++ tests/DemoTest.php
@@ -1,16 +1,23 @@
 <?php
 
-class DemoTest extends \Tests\TestCase
+class DemoTest extends \Orchestra\Testbench\TestCase
 {
     public function testSomethingIsTrue()
     {
         $this->assertTrue(true);
     }
 
-    public function testClassRegistered()
-    {
-        $this->app->singleton(\Manuelgeek\LaravelBtoast\BToast::class, function () {
-            $this->assertTrue(true);
-        });
-    }
+//    public function testClassRegistered()
+//    {
+//        $this->app->singleton(\Manuelgeek\LaravelBtoast\BToast::class, function () {
+//            $this->assertTrue(true);
+//        });
+//    }
+
+//    protected function getPackageProviders($app)
+//    {
+//        return [
+//            'Manuelgeek\LaravelBtoast\BToastServiceProvider'
+//        ];
+//    }
 }
